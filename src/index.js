f34cadd2ecae3c2a0d22bd04cb4f6af32ade9861
Eusebie Siebenberg <siebenee@gmail.com>
2023-04-21 11:01:42 -0700

    edit syntax and spelling error on index.js

--- src/index.js
+++ src/index.js
@@ -6,18 +6,18 @@ import CurrencyService from './currency-service';
 // Business Logic
 
 async function getCurrency(usdInput, currencyOutput, exchange) {
-  const promise = await CurrencyService.getCurrency(usdInput, currencyOutput, exchange);
+  const response = await CurrencyService.getCurrency(usdInput, currencyOutput, exchange);
   if (response.results) {
     printElements(response, usdInput, currencyOutput, exchange);
   } else {
-    printError(responise, usdInput, currencyOutput, exchange);
+    printError(response, usdInput, currencyOutput, exchange);
   }
 }
 
 // UI Logic
 
 function printElements(response, usdInput, currencyOutput, exchange) {
-  document.querySelector('#showResponse').innerText = `Your ${usdInput} exchange rate is ${response.conversion_rate} to $.`;
+  document.querySelector('#showResponse').innerText = `Your ${usdInput} exchange rate is ${response.conversion_rate} ${currencyOutput}, and comes to ${exchange} to $.`;
 }
 
 function printError(error, usdInput, currencyOutput) {
