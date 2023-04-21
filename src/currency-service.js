7610ef4b5278d9012abf6934b1b72e70960ae009
Eusebie Siebenberg <siebenee@gmail.com>
2023-04-21 11:17:44 -0700

    working thru request url, getting denied access

--- src/currency-service.js
+++ src/currency-service.js
@@ -1,7 +1,7 @@
 export default class CurrencyService {
-  static async getCurrency(usdInput, currencyOutput, exchange) {
+  static async getCurrency(usdInput, currencyOutput, results) {
     try {
-      const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/${usdInput}/${currencyOutput}/${exchange}`);
+      const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/${usdInput}/${currencyOutput}/${results}`);
       const jsonifiedResponse = await response.json();
       if (!response.ok) {
         const errorMessage = `${response.status} ${response.statusText} ${jsonifiedResponse.message}`;
