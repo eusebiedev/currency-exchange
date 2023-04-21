0cbf419edd3d7a5293be26e4d021430c139f5343
Eusebie Siebenberg <siebenee@gmail.com>
2023-04-21 11:58:41 -0700

    WIP: update request url to use promise, lunch break

--- src/currency-service.js
+++ src/currency-service.js
@@ -1,7 +1,7 @@
 export default class CurrencyService {
-  static async getCurrency(base_code, conversion_rates, currencyOutput, results) {
+  static async getCurrency() {
     try {
-      const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/CAD`);
+      const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`);
       const jsonifiedResponse = await response.json();
       if (!response.ok) {
         const errorMessage = `${response.status} ${response.statusText} ${jsonifiedResponse.message}`;
