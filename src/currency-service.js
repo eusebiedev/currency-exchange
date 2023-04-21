c74cf6ff202a1c045ef0ae88f80735e61c6f029c
Eusebie Siebenberg <siebenee@gmail.com>
2023-04-21 16:47:55 -0700

    Clean up indent errors on currency-service.js

--- src/currency-service.js
+++ src/currency-service.js
@@ -1,15 +1,18 @@
 export default class CurrencyService {
-  static async getCurrency() {
-    try {
-      const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`);
-      const jsonifiedResponse = await response.json();
-      if (!response.ok) {
-        const errorMessage = `${response.status} ${response.statusText} ${jsonifiedResponse.message}`;
-        throw new Error(errorMessage);
-      }
-      return jsonifiedResponse;
-    } catch(error) {  
-      return error;
-    }
+  static getCurrency(currency, currencyExchange) {
+    return new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD/`;
+      request.addEventListener("loadend", function() {
+        const response = JSON.parse(this.responseText);
+        if (this.status === 200) {
+          resolve([response, currency, currencyExchange]);
+        } else {
+          reject([this, response, currency, currencyExchange]);
+        }
+      });
+      request.open("GET", url, true);
+      request.send();
+    });
   }
 }
