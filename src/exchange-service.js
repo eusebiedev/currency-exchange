da26abad62620e8e6e78e6ce754b7150a8faf7fe
Eusebie Siebenberg <siebenee@gmail.com>
2023-04-23 16:40:37 -0700

    add variables for currency, currencyExchange to request url

--- src/exchange-service.js
+++ src/exchange-service.js
@@ -1,8 +1,8 @@
-export default class CurrencyConverter {
-  static getCurrency(currency, currencyExchange) {
+export default class CurrencyConverter{
+  static returnExchange(currency, currencyExchange) {
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`;
+      const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/${currency}/${currencyExchange}`;
       request.addEventListener("loadend", function() {
         const response = JSON.parse(this.responseText);
         if (this.status === 200) {
