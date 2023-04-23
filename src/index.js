e9454b6021bedfff7d150113fcfb9494a504471a
Eusebie Siebenberg <siebenee@gmail.com>
2023-04-23 16:41:45 -0700

    add conversion_result to index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import CurrencyService from './currency-service';
+import CurrencyConverter from './exchange-service';
 
 // Business Logic
 
-function getCurrency(currency, currencyExchange) {
-  let promise = CurrencyService.getCurrency(currency, currencyExchange);
+function returnExchange(currency, currencyExchange) {
+  let promise = CurrencyConverter.returnExchange(currency, currencyExchange);
   promise.then(function(response) {
-    printElements(response, currency, currencyExchange);
+    printElements(response);
   }, function(errorMessage) {
     printError(errorMessage);
   });
@@ -17,20 +17,20 @@ function getCurrency(currency, currencyExchange) {
 // UI Logic
 
 function printError(error) {
-  document.querySelector('#showResponse').innerText = `There was an error converting the currency data: ${error}`;
+  document.querySelector('#showResponse').innerText = `There was an error converting the currency data: ${error} ${error[0].status} ${error[1].status}`;
 }
 
-function printElements(response, currency, currencyExchange) {
-  document.querySelector('#showResponse').innerText = `Your ${currency}$ exchange rate in ${currencyExchange} is ${response.conversion_rates}`;
+function printElements(response,) {
+  document.querySelector('#showResponse').innerText = `Your ${response[1]}$ exchange rate in ${response[2]} is ${response[0].conversion_result}`;
 }
 
 function handleFormSubmission(event) {
   event.preventDefault();
-  const currency = document.querySelector('#usd-input').value;
+  const currency = document.querySelector('#currency').value;
   const currencyExchange = document.querySelector('#currencyExchange').value;
-  document.querySelector('#usd-input').value = null;
+  document.querySelector('#currency').value = null;
   document.querySelector('#currencyExchange').value = null;
-  getCurrency(currency, currencyExchange);
+  returnExchange(currency, currencyExchange);
 }
 
 window.addEventListener("load", function() {
