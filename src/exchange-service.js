d649b46ceebd20b445a85354eb1d48fc63056a22
Eusebie Siebenberg <siebenee@gmail.com>
2023-05-01 07:08:17 -0700

    edit api request to include new currency option

--- src/exchange-service.js
+++ src/exchange-service.js
@@ -1,10 +1,10 @@
 export default class CurrencyConverter{
-  static async returnExchange(currencyExchange) {
+  static async returnExchange(currency, currencyExchange, currencyExchangeOut) {
     try {
-      const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD/`);
+      const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/${currency}/${currencyExchange}/${currencyExchangeOut}/`);
       const jsonifiedResponse = await response.json();
       if (!response.ok) {
-        const errorMessage = `${response.status} ${response.statusText} ${currencyExchange.message} ${jsonifiedResponse.message}`;
+        const errorMessage = `${response.status} ${response.statusText} ${jsonifiedResponse.message}`;
         throw new Error(errorMessage);
       } 
       return jsonifiedResponse;
